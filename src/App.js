abae698ac1892a30fa69227e816f6a482d3cfb2a
hawraakhalil <hawraakhalil.05@gmail.com>
2023-08-22 01:53:01 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Button} from 'react-bootstrap';
 import './App.css';
-
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Card from 'react-bootstrap/Card'  
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from Vs2</h1>
-        <Button variant="contained">Contained</Button>
-      </header>
-    </div>
+  
+
+<div class="card" style={{maxWidth: "20rem", backgroundColor: "lightblue"}}>
+  <div class="card-body">
+
+  <h1 class="card-title">Post title #1 </h1>
+  <h6 class="card-subtitle mb-2 text-muted">Post Preview</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+  </div>
+</div>
+
+     
   );
 }
 
---
+++ src/components/rectangle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+const rectangle = () => {
+  return (
+    <div class="container">
+      <h2>whatever</h2>
+      <p>blablablablabla</p>
+      </div>
+    );
+};
+
+export default rectangle;
