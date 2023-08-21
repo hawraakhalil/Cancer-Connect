066ff2b022dbb59ebd46e49b35fff0bdf4202740
hawraakhalil <hawraakhalil.05@gmail.com>
2023-08-21 14:14:22 +0300

    changes for v2 again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './components/rectangle.html'
+import './components/rectangle.css'
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from Vs2</h1>
+        <div>hi</div>
       </header>
     </div>
   );
