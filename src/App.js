e1f506be12e6bf0657a8c2c288c931c59391b715
hawraakhalil <hawraakhalil.05@gmail.com>
2023-08-22 13:32:15 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -8,20 +8,39 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
+
+
+const posts = [
+  {
+    title:"Post title #1",
+    subtitle: "Post Preview",
+    content: "Some quick example text to build on the card title and make up the bulk of the card's content."
+  },
+  {
+    title: "Post title #2",
+    subtitle: "Another Post Preview",
+    content: "This is another example post content."
+  }
+];
+
 function App() {
   return (
-  
 
-<div class="card" style={{maxWidth: "20rem", backgroundColor: "lightblue"}}>
-  <div class="card-body">
-
-  <h1 class="card-title">Post title #1 </h1>
-  <h6 class="card-subtitle mb-2 text-muted">Post Preview</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-  </div>
-</div>
-
+<Container>
+{posts.map((post, index) => (
+<Card key={index} className="mt-4" style={{ maxWidth: "90rem", backgroundColor: "lightblue" }}>
+  <Card.Body>
+    <div className="row">
+      <div className="col-sm-6">
+        <h1 className="card-title">{post.title}</h1>
+        <h6 className="card-subtitle mb-2 text-muted">{post.subtitle}</h6>
+        <p className="card-text">{post.content}</p>
+      </div>
+    </div>
+  </Card.Body>
+</Card>
+))}
+</Container>
      
   );
 }
