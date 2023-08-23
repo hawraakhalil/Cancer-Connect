57f24e537ca85801ef56f23e315cdad602b39e37
hawraakhalil <hawraakhalil.05@gmail.com>
2023-08-24 00:08:45 +0300

    Edited the search bar and buttons to look more like the design

--- src/App.js
+++ src/App.js
@@ -40,27 +40,29 @@ function App() {
 
         id="search"
         variant="outlined"
-        label="search"
-        placeholder="  Search"
+        placeholder="  Search" 
         InputProps={{
-          startAdornment: <SearchIcon />}}
-          style={{width:"62rem", marginTop:"20px", marginLeft:"60px"}}
+          startAdornment: <SearchIcon style={{ color: "#4EA4F3" }} />,
+          style: { borderRadius: "30px" } , 
+          inputProps: { style: { color: "#4EA4F3" ,fontSize: "18px"} } }}
+          style={{width:"62rem", marginTop:"20px", marginLeft:"60px",backgroundColor:"white",borderRadius: "30px", borderColor: "white"}}
+          
       />
-            <Button style={{height:"50px",width:"130px",marginLeft:"4%", borderRadius:"30px",borderColor:"#A4D6D3",   position: "relative", top: "-110px", color: "#6495ED"}}> 
+            <Button style={{height:"50px",width:"130px",marginLeft:"4%", borderRadius:"30px",borderColor:"#A4D6D3",   position: "relative", top: "-110px", color: "#4EA4F3",backgroundColor:"white",fontWeight:"bold"}}> 
        View Account
        </Button>
 </Container>
 </header>
 
 
-<Container style={{marginTop:"60px", marginBottom: "60px" }}>
+<Container style={{marginTop:"50px", marginBottom: "50px"}}>
 {posts.map((post, index) => (
 <Card key={index} className="mt-4" style={{marginLeft:"100px",marginRight:"100px", backgroundColor: "#D7ECEC",borderRadius:"30px" }}>
   <Card.Body>
     <div className="row">
       <div className="col-sm-6">
-        <h1 className="card-title" style={{ marginTop: "30px",marginLeft: "25px",marginBottom:"5px" }}>{post.title}</h1>
-        <h6 className="card-subtitle mb-2 text-muted"  style={{marginTop: "0",marginLeft: "25px" ,marginBottom: "100px"}} >{post.subtitle}</h6>
+        <h1 className="card-title" style={{ marginTop: "30px",marginLeft: "25px",marginBottom:"5px"}}>{post.title}</h1>
+        <h6 className="card-subtitle mb-2 text-muted"  style={{marginTop: "0",marginLeft: "25px" ,marginBottom: "120px"}} >{post.subtitle}</h6>
         <p className="card-text" style= {{marginLeft: "25px"}}>{post.content}</p>
       </div>
     </div>
@@ -74,14 +76,14 @@ function App() {
 <Form >
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
 
-        <Form.Control type="text" style={{ width: "78%",marginTop: "1%",marginLeft: "7.5%",marginRight:"5%",height:"40px",fontSize: "15px" }} placeholder="New Post Title Here" />
+        <Form.Control type="text" style={{ width: "78%",marginTop: "1%",marginLeft: "7.5%",marginRight:"5%",height:"40px",fontSize: "15px" }} placeholder="       New Post Title Here" />
         
       </Form.Group>
       <br />
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Control as="textarea" style={{ width: "78%",marginLeft: "7.5%",marginRight:"5%",height:"100px" ,fontSize: "15px"}}  placeholder="New Post Message Here"  />
-       <Button style={{ marginTop: "-100%" , marginBottom:"1%" ,height:"120px",width:"150px",marginLeft:"-3%", borderRadius:"30px",borderColor:"#A4D6D3"}}> 
-       <SendIcon style={{fontSize: "50px",height:"120px",color: "#A4D6D3" }}> </SendIcon>
+        <Form.Control as="textarea" style={{ width: "78%",marginLeft: "7.5%",marginRight:"5%",height:"100px" ,fontSize: "15px"}}  placeholder="    New Post Message Here"  />
+       <Button style={{ marginTop: "-100%" , marginBottom:"1%" ,marginTop:"-50px",height:"120px",width:"140px",marginLeft:"-3%", borderRadius:"30px",borderColor:"#A4D6D3",backgroundColor:"white"}}> 
+       <SendIcon style={{height:"120px",width:"75px",color: "#A4D6D3" ,marginLeft:"5px"}}> </SendIcon>
        </Button>
       </Form.Group>
     </Form>
