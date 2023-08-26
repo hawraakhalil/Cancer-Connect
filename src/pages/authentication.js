63c2ce4c94dfd72ffda8c27b5aa4a8a36b261f2b
hawraakhalil <hawraakhalil.05@gmail.com>
2023-08-26 16:49:20 +0300

    Added the username and password textfields

--- src/pages/authentication.js
+++ src/pages/authentication.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../App.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Form from 'react-bootstrap/Form';
+import images from './images.png';
 
 const handleClick = () => {
     window.location.href = "/feed";
   };
 
 function Authentication() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
     return (
         <>
     <Container>
       <Card style={{ width: '35rem',height:"28rem", backgroundColor:"#A4D6D3" , marginLeft:"31%", marginTop:"8%", borderRadius:"5rem"}}>
-      <Card.Body>
-      <Card.Header as="h5" style={{ fontFamily: 'Quicksand',color: '#155A56', position: 'relative', top: '11rem', left: '11rem',fontSize:"2.7rem" ,fontWeight: "800" }}>Welcome!</Card.Header>
-        <Button onClick={handleClick} style ={{marginTop:"10rem",marginLeft:"12rem"}}>Go to Feed here for now</Button>
+      <img src={images} alt="Logo" className="rounded-circle" style={{ borderRadius: "50rem",height: "8rem", width: "8rem",marginLeft:"38%",marginTop:"1rem"}} />
+      <Card.Body style ={{marginTop:"-11rem"}}>
+      <Card.Header as="h5" style={{ fontFamily: 'Quicksand',color: '#155A56', position: 'relative', top: '11rem', left: '11rem',fontSize:"2.7rem" ,fontWeight: "700" ,marginTop:"1rem"}}>Welcome!</Card.Header>
+      <div>
+      <Form.Control
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="        Username" 
+        style={{marginTop:"20%",marginLeft:"20%",width:"20rem",height:"2.8rem",marginBottom:"0.5rem",borderColor:"#FFFFFF",borderRadius:"1.5rem",fontFamily:"Lato",fontWeight:"bold"}}
+      />
+      </div>
+      <div>
+      <Form.Control
+        type="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="       Password"
+        style ={{marginLeft:"20%", width:"20rem",height:"2.8rem",borderRadius:"1.5rem",borderColor:"#FFFFFF",fontFamily:"Lato",fontWeight:"bold"}}
+      />
+      <Button onClick={handleClick} style ={{marginTop:"1.5rem",marginLeft:"13.5rem",width:"8rem",height:"3rem",backgroundColor:"#FFE3F4",borderColor:"#FFE3F4",borderRadius:"10rem",fontFamily:"Lato",fontWeight:"bold"}}>Login</Button>
+      </div>
+      
       </Card.Body>
     </Card>
     </Container>
